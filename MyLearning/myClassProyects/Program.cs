9eaeea3432d49e2ae000cc5877fe4bc449393606
Raymer292 <raymer29f@gmail.com>
2026-02-06 23:49:52 -0400

    questionnaire 1

--- MyLearning/myClassProyects/Program.cs
+++ MyLearning/myClassProyects/Program.cs
@@ -1,2 +1,151 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+using static System.Net.Mime.MediaTypeNames;
+{ 
+//string text = "palabra o frase";
+//char caracter = 'A';
+//bool booleano = false;
+//int ints = 28;
+//double desima = 28.2007;
+//float desimaF = 28.2007f;
+//decimal maxDesima = 28.2007m;
+//const int constant = 2020;
+
+//Console.WriteLine("ejemplos de variables: \n");
+
+//Console.WriteLine($"string: {text}");
+//Console.WriteLine($"char: {caracter}");
+//Console.WriteLine($"bool: {booleano}");
+//Console.WriteLine($"int: {ints}");
+//Console.WriteLine($"double: {desima}");
+//Console.WriteLine($"float: {desimaF}");
+//Console.WriteLine($"decimal: {maxDesima}");
+//Console.WriteLine($"const: {constant}");
+
+}
+
+{
+    //const string ABSOLUTE = "ABSOLUTE";
+
+    //Console.WriteLine($" intenta modificar la palabra: {ABSOLUTE} ");
+
+
+    //string tries = Console.ReadLine();
+
+    //if(tries != ABSOLUTE)
+    //{
+
+    //}
+
+}
+
+{ 
+//int testing = 10;
+//int variable = 0;
+
+//int salidad = 0;
+
+//Console.WriteLine($"el numero inicial es {testing} has por lo menos una operacion con el");
+
+ 
+
+//do
+//{
+// Console.WriteLine("que operacion deseas hacer \n 1-suma , 2-resta , 3-murtiplicacion , 4-divicion , 5 salir/comprovar numero ");
+
+//    salidad = Convert.ToInt32(Console.ReadLine());
+    
+//    switch(salidad)
+//    {
+//        case 1:
+//        {
+//                variable = Convert.ToInt32(Console.ReadLine());
+//                testing += variable;
+
+//                Console.WriteLine($"numero: {testing}");
+//                Console.ReadKey();
+//                Console.Clear();
+
+//            }
+//            break;
+//        case 2: 
+//            {
+//                variable = Convert.ToInt32(Console.ReadLine());
+//                testing -= variable;
+//                Console.WriteLine($"numero: {testing}");
+//                Console.ReadKey();
+//                Console.Clear();
+
+//            }
+//            break;
+//        case 3: 
+//            {
+//                variable = Convert.ToInt32(Console.ReadLine());
+//                testing *= variable;
+//                Console.WriteLine($"numero: {testing}");
+//                Console.ReadKey();
+//                Console.Clear();
+
+//            }
+//            break;
+//        case 4: 
+//            {
+//                variable = Convert.ToInt32(Console.ReadLine());
+//                testing /= variable;
+//                Console.WriteLine($"numero: {testing}");
+//                Console.ReadKey();
+//                Console.Clear();
+//            }
+//            break;
+
+//        case 5:
+//            {
+
+//                Console.WriteLine($"buen trabajo tu numero era 10 Y AHORA es {testing}");
+// }
+//            break;
+
+
+//    }
+
+//} while ( salidad < 5 );
+
+}
+
+{
+    //float Valoz = 10152466.25f;
+
+    //byte testX = 5;
+
+    //Console.WriteLine($"prueva {testX}");
+}
+
+
+{
+    
+
+//comando/inpresion basica en cualquier lenguaje hola mundo
+//Console.WriteLine("hello world");
+// ejemplo de comentario en linea de codigo
+
+/*
+int[] ints = new int[5];
+
+Console.WriteLine("ingrese 5 numeros ");
+
+for (int x = 0; x <= 4; x++)
+{
+
+    ints[x] = Convert.ToInt32(Console.ReadLine());
+}
+
+ejemplo de comentario de columnas
+*/
+
+//DateTime time = DateTime.Now;
+
+//Console.WriteLine($"la fecha y hora acctual es {time}");
+
+
+   }
+
