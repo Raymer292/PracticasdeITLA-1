4c32559833805df1b6a6f6348371166332668700
Raymer292 <raymer29f@gmail.com>
2026-02-06 20:34:18 -0400

    testing for and foreach

--- MyLearning/proyecto Pr.1/Program.cs
+++ MyLearning/proyecto Pr.1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] ints = new int[5];
+
+Console.WriteLine("ingrese 5 numeros ");
+
+for (int x = 0; x <= 4; x++)
+{
+   
+    ints[x] = Convert.ToInt32(Console.ReadLine());
+}
+//buscar una forma de invertilo mejor que el reverse
+foreach (int i in ints.Reverse())
+{
+    Console.WriteLine("sus numeros fueron");
+    Console.WriteLine(ints[i-1]);
+}
---
+++ MyLearning/proyecto Pr.1/mynewclass1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proyecto_Pr._1
+{
+    internal class mynewclass1
+    {
+    }
+}
